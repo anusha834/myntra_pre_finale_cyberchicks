742dc206a881c983074462f7bd8ce5c93f73a3c1
anusha834 <110539334+anusha834@users.noreply.github.com>
2024-07-30 23:58:08 +0530

    Update RecommendationsPage.jsx

--- src/pages/RecommendationsPage.jsx
+++ src/pages/RecommendationsPage.jsx
@@ -1,49 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import axios from 'axios';
-// import navbarimg from '../assets/nav_bar.jpg';
-// import sidebar from '../assets/sidebar.jpg';
-// import '../styles/RecommendationsPage.css'; 
-// const RecommendationsPage = () => {
-//   const { id } = useParams();
-//   const [recommendations, setRecommendations] = useState([]);
 
-//   useEffect(() => {
-//     axios.get(`http://localhost:8000/api/items/${id}/recommendations/`)
-//       .then(response => setRecommendations(response.data))
-//       .catch(error => console.error('Error fetching recommendations:', error));
-//   }, [id]);
-
-//   return (
-//     <div>
-//        <header>
-//         <img className='navbar' src={navbarimg} alt="Navbar" />
-//       </header>
-//       <div className='content'>
-//       <aside>
-//           <img src={sidebar} alt="Sidebar" />
-//         </aside>
-//         <main>
-//       <h1>Recommendations</h1>
-      
-//       <div className="image-gallery">
-//         {recommendations.map((rec, index) => (
-//           <img
-//             key={index}
-//             src={`http://localhost:8000${rec.image}`}
-//             alt={`Recommendation ${index}`}
-//             style={{ width: '200px', height: 'auto', margin: '10px' }}
-//           />
-//         ))}
-//         </div>
-//         </main>
-//       </div>
-      
-//     </div>
-//   );
-// };
-
-// export default RecommendationsPage;
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import axios from 'axios';
