fc28fbf0600747d2f9fbae9a6b66fbfb7cc38421
anusha834 <110539334+anusha834@users.noreply.github.com>
2024-07-31 00:11:19 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,44 @@
+"""
+URL configuration for ecom project.
 
-from django.urls import path
-from .views import ClothingItemList, ClothingItemDetail, ClothingItemRecommendations
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 
+
+# fashion_recommender/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
-    path('items/', ClothingItemList.as_view(), name='clothing-item-list'),
-    path('items/<int:id>/', ClothingItemDetail.as_view(), name='clothing-item-detail'),
-    path('items/<int:id>/recommendations/', ClothingItemRecommendations.as_view(), name='clothing-item-recommendations'),
-
+    path('admin/', admin.site.urls),
+    path('api/', include('classify.urls')),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
+ 
+
+# from django.contrib import admin
+# from django.urls import path,include
+# from django.urls import path
+# from classify.views import get_all_outfits, get_recommendations
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('outfits/<str:category>/', get_all_outfits, name='get_all_outfits'),
+#     path('recommendations/', get_recommendations, name='get_recommendations'),
+# ]
+
+
 
 
